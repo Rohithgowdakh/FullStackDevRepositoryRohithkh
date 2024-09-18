f4020fadd8bea79648cbe70adaff173a39389f89
Rohithgowdakh <rohithkh2020@gmail.com>
2024-09-18 19:32:15 +0530

    Spring User ResponseEntity

---
+++ user/src/main/java/com/gentech/user/service/UserService.java
@@ -0,0 +1,18 @@
+package com.gentech.user.service;
+
+import java.util.List;
+
+import com.gentech.user.entity.User;
+
+public interface UserService {
+	
+	User createUser(User user);
+	
+	List<User> getUsers();
+	
+	User getSpecificUser(Long id);
+	
+	User updateUser(User user,Long id);
+	
+	void deleteUser(Long id);
+}
